1d3e4f11cb3a7a13ce489f189fdc2420b7b0ca59
Meftahul islam <islam1815020@stud.kuet.ac.bd>
2023-11-30 01:35:59 +0600

    final

--- src/context/SocketProvider.jsx
+++ src/context/SocketProvider.jsx
@@ -9,7 +9,7 @@ export const useSocket = () => {
 };
 
 export const SocketProvider = (props) => {
-  const socket = useMemo(() => io(process.env.REACT_APP_URL), []);
+  const socket = useMemo(() => io("https://wave-faceted-macadamia.glitch.me"), []);
 
   return (
     <SocketContext.Provider value={socket}>
